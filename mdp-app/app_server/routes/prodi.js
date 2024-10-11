2140d582062698da6fb6336c906e90f799db081d
nabilasusan <nabila_2226240101@mhs.mdp.ac.id>
2024-10-11 11:15:44 +0700

    pppp

---
+++ mdp-app/app_server/routes/prodi.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+
+// import controller
+const prodiController = require('../controllers/prodiControllers')
+module.exports = router;
