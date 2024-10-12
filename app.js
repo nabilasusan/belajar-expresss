798017da517b13b8db217f1c941c13e8ecd517f5
nabilasusan <nabila_2226240101@mhs.mdp.ac.id>
2024-10-12 13:04:14 +0700

    update

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'app_server','views')));
 app.set('view engine','ejs');
-
+app.use(expressLayout);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
